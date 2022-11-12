882e78b5f6e3bd98069dd5a48becdc58c3fb8d30
Lu Kotova <loui.kotova@gmail.com>
2022-11-12 22:38:06 +0100

    add homework part4

--- src/homework8/Array.java
+++ src/homework8/Array.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Array {
+
+    private static Random random = new Random();
+
     static void process(int[] input) {
         int count = 0;
         for (int i = 0; i < input.length; i++) {
             if (input.length == 3) {
-                input[i] = new Random().nextInt(109);
+                input[i] = random.nextInt(109);
             } else if (input.length < 3) {
                 input[i] = new Random().nextInt(109) * 2;
             } else {
@@ -29,17 +32,42 @@ public class Array {
 
     static void fillInReverse(int[] input) {
         for (int i = input.length - 1; i >= 0; i--) {
-            input[i] = new Random().nextInt(109);
+            input[i] = random.nextInt(109);
         }
     }
 
     static void fillTwoOverTwo(int[] input) {
         for (int i = 0; i < input.length; i++) {
             if (i % 4 < 2) {
-                input[i] = new Random().nextInt(109);
+                input[i] = random.nextInt(109);
             }
         }
     }
+    static void fillInMore(int[] input) {
+        int cycleLength = 1;
+        int step = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (step < cycleLength){
+                input[i] = random.nextInt(109);
+                step++;
+            } else {
+                step = 0;
+                i+= cycleLength - 1;
+                cycleLength++;
+            }
+        }
+    }
+
+    static void fillInEven(int[] input){
+        int index = 0;
+        while (index < input.length){
+            input[index] = random.nextInt(109) * 2;
+            index++;
+        }
+    }
+
+
+
 
     public static void main(String[] args) {
         int[] array1 = {23, 45, 67};
@@ -57,5 +85,11 @@ public class Array {
         int[] array4 = new int[21];
         fillTwoOverTwo(array4);
         System.out.println(Arrays.toString(array4));
+        int[] array5 = new int[31];
+        fillInMore(array5);
+        System.out.println(Arrays.toString(array5));
+        int[] array6 = new int[15];
+        fillInEven(array6);
+        System.out.println(Arrays.toString(array6));
     }
 }
