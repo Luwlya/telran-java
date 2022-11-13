6037cc42d237350ce919faa8bc9971f3ef832ae0
Lu Kotova <loui.kotova@gmail.com>
2022-11-13 22:20:42 +0100

    add homework switchcase

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -3,9 +3,9 @@ package calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    static int calculate(int first, String action, int second){
+    static int calculate(int first, String action, int second) {
         int result = 0;
-        if (action.equals("+")){
+        if (action.equals("+")) {
             result = first + second;
         } else if (action.equals("-")) {
             result = first - second;
@@ -14,18 +14,39 @@ public class Calculator {
         } else if (action.equals("/")) {
             result = first / second;
         } else {
-            System.out.println("Wrong action: "+ action);
+            System.out.println("Wrong action: " + action);
+        }
+        return result;
+    }
+
+    static int calculateInSwitch(int first, String action, int second) {
+        int result = 0;
+        switch (action) {
+            case "+":
+                result = first + second;
+                break;
+            case "-":
+                result = first - second;
+                break;
+            case "*":
+                result = first * second;
+                break;
+            case "/":
+                result = first / second;
+                break;
+            default:
+                System.out.println("Wrong action: " + action);
         }
         return result;
     }
 
     public static void main(String[] args) {
-        while(true) {
+        while (true) {
             Scanner scanner = new Scanner(System.in);
             int first = scanner.nextInt();
             String action = scanner.next();
             int second = scanner.nextInt();
-            int result = calculate(first,action,second);
+            int result = calculateInSwitch(first, action, second);
             System.out.println(result);
         }
     }
